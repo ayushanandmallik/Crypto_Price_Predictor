2858939bc78a715060b91865e8ccd3ce541943ae
Ayush Anand Mallik <ayushmallik6342@gmail.com>
2021-11-09 22:13:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ from pages import predict, prices
 #creating an instance of the app
 app= MultiPage()
 
-#title of the main page
-#st.title('CryptoCurrency Price Prediction')
+
 
 #adding all the pages to the website
 app.add_page('Predict the future price',predict.app)
